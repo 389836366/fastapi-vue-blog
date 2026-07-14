ce8241b41fff6e0319960c939d277c4a38cdda07
yongyang <2955520958@qq.com>
2026-07-14 17:18:01 +0800

    chore: practice JavaScript variables

--- frontend/main.js
+++ frontend/main.js
@@ -1,1 +1,22 @@
 console.log("个人博客项目启动成功");
+
+const blogName = "yongyang的博客";
+let articleCount = 3;
+const isLearning = true;
+
+console.log(blogName);
+console.log(articleCount);
+console.log(isLearning);
+
+articleCount = articleCount + 1;
+
+console.log("博客名称：" + blogName);
+console.log("文章数量：" + articleCount);
+console.log("正在学习：" + isLearning);
+
+let studyDays = 4;
+console.log(studyDays);
+studyDays = studyDays + 1;
+console.log(studyDays);
+const currentTopic = "JavaScript基础";
+console.log("当前学习内容："+ currentTopic);
